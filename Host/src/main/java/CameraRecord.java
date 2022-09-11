366dde80075de4870810c4d610f55fae4498cd68
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2022-09-10 22:47:13 -0400

    Added recordId and tab space was changed
    The number of spaces in a Tab was changed in IntelliJ from 2 to 4 spaces (4 is default). Auto Format changed this file. Also, the recordId was added because it was needed to update the database camera_log records with isNew = 0;

--- Host/src/main/java/CameraRecord.java
+++ Host/src/main/java/CameraRecord.java
@@ -1,20 +1,26 @@
 public class CameraRecord {
-  private final int cameraId;
-  private final int changedSpaces;
+    private final int recordId;
+    private final int cameraId;
+    private final int changedSpaces;
 
-  // Constructor
-  public CameraRecord(int id, int spaces) {
-    cameraId = id;
-    changedSpaces = spaces;
-  }
+    // Constructor
+    public CameraRecord(int id1, int id2, int spaces) {
+        recordId = id1;
+        cameraId = id2;
+        changedSpaces = spaces;
+    }
 
-  public int getCameraId() {
-    return cameraId;
-  }
+    public int getRecordId() {
+        return recordId;
+    }
 
-  public int getChangedSpaces() {
-    return changedSpaces;
-  }
+    public int getCameraId() {
+        return cameraId;
+    }
+
+    public int getChangedSpaces() {
+        return changedSpaces;
+    }
 
 
 }
