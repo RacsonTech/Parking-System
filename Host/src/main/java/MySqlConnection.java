9294dd4b2274a4b94544770fae40919db7d86d16
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2022-09-10 22:51:18 -0400

    env variables were renamed.

--- Host/src/main/java/MySqlConnection.java
+++ Host/src/main/java/MySqlConnection.java
@@ -1,4 +1,5 @@
 import com.mysql.cj.jdbc.MysqlDataSource;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
@@ -6,66 +7,60 @@ import java.util.Properties;
 import java.sql.SQLException;
 
 
+public class MySqlConnection {
+    private String mySqlUrl;
+    private String mySqlUser;
+    private String mySqlPassword;
+    private MysqlDataSource dataSource = null;
+    private Connection connection;
 
-public class MySqlConnection
-{
-  private String mySqlUrl;
-  private String mySqlUser;
-  private String mySqlPassword;
-  private MysqlDataSource dataSource = null;
+    public MySqlConnection() throws SQLException {
 
-  private Connection connection;
+        // Reading the MySQL credentials from the dp.properties file
+        System.out.println("Reading MySQL Credentials");
+        readMySqlCredentials();
 
-  public MySqlConnection() throws SQLException {
+        // Prepare connection to the local MySQL DB
+        System.out.println("Preparing Connection to MySQL");
+        prepareConnection();
 
-    // Reading the MySQL credentials from the dp.properties file
-    System.out.println("Reading MySQL Credentials");
-    readMySqlCredentials();
-
-    // Prepare connection to the local MySQL DB
-    System.out.println("Preparing Connection to MySQL");
-    prepareConnection();
-
-    // Establish a connection to the database
-    System.out.println("Establishing a connection to MySQL.");
-    assert dataSource != null;
-    connection = dataSource.getConnection();
-    System.out.println("Connection established.\n");
-
-  }
-
-  public Connection getConnection() {
-    return connection;
-  }
-
-  private void readMySqlCredentials() {
-
-    // Read MySQL credentials from the properties file
-    Properties properties = new Properties();
-    FileInputStream inputFile;
-
-
-    try {
-      inputFile = new FileInputStream("src\\main\\resources\\db.properties");
-      properties.load(inputFile);
-
-      mySqlUrl = properties.getProperty("MYSQL_DB_URL");
-      mySqlUser = properties.getProperty("MYSQL_DB_USERNAME");
-      mySqlPassword = properties.getProperty("MYSQL_DB_PASSWORD");
-
-      inputFile.close();
-      properties.clear();
-
-    } catch (IOException e) {
-      e.printStackTrace();
+        // Establish a connection to the database
+        System.out.print("Connecting...");
+        assert dataSource != null;
+        connection = dataSource.getConnection();
+        System.out.println("Connected.\n");
     }
-  }
 
-  private void prepareConnection() {
-    dataSource = new MysqlDataSource();
-    dataSource.setURL(mySqlUrl);
-    dataSource.setUser(mySqlUser);
-    dataSource.setPassword(mySqlPassword);
-  }
+    public Connection getConnection() {
+        return connection;
+    }
 
+    private void readMySqlCredentials() {
+
+        // Read MySQL credentials from the properties file
+        Properties properties = new Properties();
+        FileInputStream inputFile;
+
+        try {
+            inputFile = new FileInputStream("src\\main\\resources\\db.properties");
+            properties.load(inputFile);
+
+            mySqlUrl = properties.getProperty("JAVA_MYSQL_DB_URL");
+            mySqlUser = properties.getProperty("JAVA_MYSQL_DB_USERNAME");
+            mySqlPassword = properties.getProperty("JAVA_MYSQL_DB_PASSWORD");
+
+            inputFile.close();
+            properties.clear();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void prepareConnection() {
+        dataSource = new MysqlDataSource();
+        dataSource.setURL(mySqlUrl);
+        dataSource.setUser(mySqlUser);
+        dataSource.setPassword(mySqlPassword);
+    }
 }
