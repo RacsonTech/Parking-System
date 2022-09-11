368781b404275f158711ae24bba92660c19a1025
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2022-09-11 00:27:40 -0400

    Added private field ipAddress

--- Host/src/main/java/Display.java
+++ Host/src/main/java/Display.java
@@ -2,11 +2,13 @@ public class Display {
     private final int displayId;
     private final int sectionId;
     private final int number;
+    private final String ipAddress;
 
-    public Display(int displayId, int sectionId, int number) {
+    public Display(int displayId, int sectionId, int number, String ipAddress) {
         this.displayId = displayId;
         this.sectionId = sectionId;
         this.number = number;
+        this.ipAddress = ipAddress;
     }
 
     public int getDisplayId() {
@@ -21,4 +23,7 @@ public class Display {
         return number;
     }
 
+    public String getIpAddress() {
+        return ipAddress;
+    }
 }
