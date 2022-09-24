9e37fdc16741ed805cc2b823efc4f9c87b22f344
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2022-09-24 12:13:14 -0400

    Created GUI Practice 2

---
+++ Host2/src/main/java/com/smartparking/host/GUI_Practice_2.java
@@ -0,0 +1,39 @@
+package com.smartparking.host;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class GUI_Practice_2 extends Application {
+    final Color COLOR_MAIN = Color.SILVER;
+    final Color COLOR_LIGHT = Color.LIGHTGREY;
+    final Color COLOR_DARK = Color.web("5F5F5F");
+    final Color COLOR_CONTRAST = Color.web("232323");
+
+    final Color FONT_COLOR_DARK = Color.BLACK;
+    final Color FONT_COLOR_LIGHT = Color.WHITE;
+    final String FONT_MAIN = "Verdana";
+
+    final int WINDOW_WIDTH = 720;
+    final int WINDOW_HEIGHT = 480;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("GUI_Practice_2.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), WINDOW_WIDTH, WINDOW_HEIGHT);
+        Image icon = new Image("logo_256x256.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Smart Parking System - Control Unit Host Program");
+
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
