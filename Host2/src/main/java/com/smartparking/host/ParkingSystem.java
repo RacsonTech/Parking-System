4f778bc1abb9a31ed26aceec803cec0a4bf2e976
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2022-10-11 18:27:44 -0400

    Removed pieces of JavaFX

--- Host2/src/main/java/com/smartparking/host/ParkingSystem.java
+++ Host2/src/main/java/com/smartparking/host/ParkingSystem.java
@@ -1,11 +1,5 @@
 package com.smartparking.host;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
--- Host2/src/main/java/module-info.java
+++ Host2/src/main/java/module-info.java
@@ -1,15 +1,15 @@
 module com.smartparking.host {
-    requires javafx.controls;
-    requires javafx.fxml;
+//    requires javafx.controls;
+//    requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
+//    requires org.controlsfx.controls;
+//    requires org.kordamp.bootstrapfx.core;
 
     // Added by Oscar
     requires mysql.connector.java;
     requires java.sql;
     requires java.naming;
 
-    opens com.smartparking.host to javafx.fxml;
-    exports com.smartparking.host;
+//    opens com.smartparking.host to javafx.fxml;
+//    exports com.smartparking.host;
 }
