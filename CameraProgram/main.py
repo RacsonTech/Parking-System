5f979bad15fdc25274a0f38fb7b62aee4bbb6997
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2022-10-16 12:41:51 -0400

    Added more comments to the Camera Program

--- CameraProgram/main.py
+++ CameraProgram/main.py
@@ -1,12 +1,67 @@
+# Parking-System - Camera program
+# Python program sample
+#
+# What the script does:
+#   1) Reads the database credentials from a .env file
+#   2) Inserts data to the local MYSQL database
+#   3) Runs every 5 seconds.
+#   Note: the script simulates the camera sending data to the MySQL. It is not a copy and paste
+#         program. It needs to be modified to suit your needs. The idea is that after the camera algorithm has
+#         determined that a car entered or left the area, the camera's internal computer will run a script like
+#         this one to connect and write to the database. The code within the "main" function is what can be used.
+#         The mysql query: "INSERT INTO `camera_log` (`camera_id`, `changed_spaces`) VALUES (1, -2)" is what needs
+#         to be modified. For example, instead hard coding the value "-2", it should be a variable. It'll most likely
+#         need a placeholder in place of the -2, and the variable somewhere else.
+#         Notice that the main function reads the credentials from the .env file everytime the main function is run.
+#         It may be possible to read the credentials once.
+#
+# What is needed to run the script:
+#   1) The latest version of Python 3
+#   2) PyCharm IDE
+#   3) MySQL installed and configured with the parking system database.
+#
+# Configuring PyCharm:
+#   1) Create a new project in PyCharm with the following options:
+#       - New environment using: Virtualenv
+#       - Base interpreter: [choose the location of your python.exe executable]
+#           For example: C:\Users\[your username]\AppData\Local\Programs\Python\Python310\python.exe
+#       - Check: Create a main.py welcome script.
+#       - Then, run this script (main.py) to confirm that PyCharm is working properly (You should see
+#           "Hi, PyCharm" in PyCharm's terminal window).
+#   2) Copy and paste the camera program code into the main.py (Delete what is in the main.py)
+#   3) Install all the import packages missing (Hover your mouse pointer over any code with a red underline)
+#       - For mysql.connector: Select "more actions" >> Install package mysql-connector-python
+#       - For dotenv: Select "more actions" >> Install package python-dotenv
+#   4) Add a Database source:
+#       - To go: View >> Tools Windows >> Database
+#       - "+" >> Data Source >> MySQL
+#       - Host: localhost      <-- Note: Enter: "localhost" if your computer has the parking system DB, or enter:
+#                                   [the ip address of the mysql server] when the DB is in another computer.
+#       - Authentication: User & Password
+#       - User: [enter mysql user from the .env file]
+#       - Password: [enter password from the .env file]
+#       - Save: Forever
+#       - Database: parkingsystem
+#       - URL: [should be automatically entered by pycharm, but it should be in the form of:
+#               jdbc:mysql://localhost:3306/parkingsystem)
+#       - Click on Test Connection (Troubleshoot any issues)
+#       - Click Ok.
+#   5) Place the .env file in the root folder of this python script
+#   6) Pycharm will highlight the following code line with an error saying "Unable to resolve camera_log Table"
+#       - This error can be ignored, or you can go to: View >> Tools Windows >> Database, right-click on
+#           parkingsystem, and click refresh.
+#   7) Run the program, and it should display the following in the terminal:
+#       MySQL connection is closed
+#       1 Record inserted successfully into camera_log table
+
+
 import sched
 import time
 import mysql.connector
 from dotenv import load_dotenv
 import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# initializes a general purpose event scheduler
 s = sched.scheduler(time.time, time.sleep)
 
 
@@ -35,9 +90,15 @@ def main(sc):
             connection.close()
             print("MySQL connection is closed")
 
+    # Run the main function after 5 seconds. sc is passed as an argument, so the main function can
+    # be scheduled to run again in 5 seconds, repeating the cycle (an infinite loop).
     sc.enter(5, 1, main, (sc,))
 
 
+# s.enter(delay=2s, priority=2, Function to Call=main, argument to pass to the main function=(s,)(
+# which is the scheduler itself)
+# Basically, schedule an event which will run the main function after 2 seconds has past. So, this line of code
+# runs main once.
 s.enter(2, 1, main, (s,))
 s.run()
 
