284a6cd3c1b1f0c9766636419dff0dd58114c694
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2022-10-23 11:28:03 -0400

    Improve outputs to terminal
    Also created an executable jar file (reason the xml files were created)

--- EthernetServer/Server/src/EthernetServer.java
+++ EthernetServer/Server/src/EthernetServer.java
@@ -13,18 +13,21 @@ public class EthernetServer {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Server started");
+//        System.out.println("\nServer started at port " + port);
+        System.out.println("\nLED Display listening on port " + port);
 
         // while statement cannot complete without throwing an exception java
         // This error seems to be a false positive since Java thinks an infinite loop is a mistake.
         while(true) {
 
-            System.out.println("Waiting for a client ...");
+//            System.out.println("Waiting for a client ...");
+            System.out.println("Waiting for updates from Control Unit...");
 
             try {
 
                 cSocket = sSocket.accept();
-                System.out.println("Client accepted");
+//                System.out.println("Client accepted");
+                System.out.println("Control Unit connected");
                 out = new PrintWriter(cSocket.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
                 String data = null;
@@ -32,16 +35,17 @@ public class EthernetServer {
                 // Read string sent by client
                 data = in.readLine();
 
-                System.out.println("Received: "+ "\n");
+                System.out.println("Update Received: ");
+                System.out.println("==================");
+                System.out.println("|    " + data + "  --->    |");
+                System.out.println("==================\n");
 
-                // Sent back the string received (As a confirmation, maybe)
+                // Sent back the string received (As a confirmation)
                 out.println("Received: " + data);
 
             } catch (IOException e) {
-//                throw new RuntimeException(e);
                 System.out.println("An error has occurred.");
             }
-
         }
     }
 
@@ -54,11 +58,10 @@ public class EthernetServer {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     public static void main(String[] args) {
         EthernetServer server = new EthernetServer();
-        server.start(5000);
+        server.start(5050);
     }
 }
